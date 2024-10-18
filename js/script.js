dece8b6862a2584ebe35f219dce12662e43d88d1
Adam893-o <asharifeh@hawk.iit.edu>
2024-10-18 15:54:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- function myEventfunction () {
-    alert ('Hello');
- }
+function displayWords() {
+    // Get the value from the input field
+    let input = document.getElementById('userInput').value;
 
-
-
+    // Display the input in the output div
+    document.getElementById('output').innerHTML = input;
+}
 
 
 
